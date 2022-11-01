9f22238f15792b7028e43b30a40c44b043f1ad2d
zarya <oajasin3@gatech.edu>
2022-11-01 14:30:52 -0400

    updated configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         'GPZXgn82kYvs7r3aq5mPE7oci',
+  consumer_secret:      'w1U4rHb8cGSabngwdr1dpCiR4qpfkGXYVNyHUmYaj62pweJ0wL',
+  access_token:         '1587449142917283840-uljOXg3kWbYWMCBBIgzNIxl6zsrfKW',
+  access_token_secret:  'Tg4qdqBV0xJrNE1bTbLoX11Q8EvCNn5RJFXJe0NKH8pMo'
 }
